1d832d3850fb968745f18244acbcff7254017095
Kong Jung Yong <menistream@gmail.com>
2022-01-11 18:43:50 +0900

    Add files via upload
    test

--- app.py
+++ app.py
@@ -1,94 +1,43 @@
-from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+import json
+
+from bson.json_util import dumps
+from pymongo import MongoClient
+from flask import Flask, render_template, jsonify
+
 
 app = Flask(__name__)
 
-from pymongo import MongoClient
-
-# client = MongoClient('localhost', 27017)
-client = MongoClient('mongodb://13.125.81.75', 27017, username="test", password="test")
+client = MongoClient('mongodb://test:test@13.125.81.75', 27017)
 db = client.dbsparta_d8
 
-SECRET_KEY = 'SPARTA'
-
-import jwt
-
-# 비밀번호를 암호화하여 DB에 저장
-import hashlib
 
 @app.route('/')
 def list_main():
-    return render_template('index.html')
+    cursor = db.gameboard.find().sort('date', -1)  # date 역순(최근)
+    result = dumps(list(cursor), ensure_ascii=False)
+    return render_template('index.html', items=result) # jinja 적용
 
 
-@app.route('/post')
-def list_post():
-    return render_template('post.html')
+# 최근 날짜부터 보여주기
+@app.route('/api/list/dateOrder', methods=['GET'])
+def view_list_date_order():
+    all_lists = list(db.gameboard.find().sort('date',-1)) # date 역순(최근)
+    return jsonify({'all_lists': dumps(all_lists)}) # object id 사용하기 위해 dumps 사용
 
 
-@app.route('/detail')
-def list_detail():
-    return render_template('detail.html')
+# 좋아요가 많은 순으로 보여주기
+@app.route('/api/list/likeOrder', methods=['GET'])
+def view_list_like_order():
+    all_lists = list(db.gameboard.find().sort('likes',-1)) # 좋아요 역순(많은 순)
+    return jsonify({'all_lists': dumps(all_lists)}) # object id 사용하기 위해 dumps 사용
 
 
-@app.route('/login')
-def login():
-    msg = request.args.get("msg")
-    return render_template('login.html', msg=msg)
+# 황금밸런스만 보여주기
+@app.route('/api/list/goldenBalance', methods=['GET'])
+def view_list_golden():
+    golden_lists = list(db.gameboard.find().sort('date',-1)) # date 역순(최근)
+    return jsonify({'all_lists': dumps(golden_lists)}) # object id 사용하기 위해 dumps 사용
 
-
-@app.route('/regist')
-def register():
-    return render_template('regist.html')
-
-
-
-
-# [회원가입 API]
-# id, pw, name을 받아서, mongoDB에 저장합니다.
-# 저장하기 전에, pw를 sha256 방법(=단방향 암호화. 풀어볼 수 없음)으로 암호화해서 저장합니다.
-@app.route('/api/regist', methods=['POST'])
-def api_regist():
-    id_receive = request.form['id_give']
-    pw_receive = request.form['pw_give']
-    name_receive = request.form['name_give']
-
-    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
-
-    db.user.insert_one({'id': id_receive, 'pw': pw_hash, 'name': name_receive})
-
-    return jsonify({'result': 'success'})
-
-
-# [로그인 API]
-# id, pw를 받아서 맞춰보고, 토큰을 만들어 발급합니다.
-@app.route('/api/login', methods=['POST'])
-def api_login():
-    id_receive = request.form['id_give']
-    pw_receive = request.form['pw_give']
-
-    # 회원가입 때와 같은 방법으로 pw를 암호화합니다.
-    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
-
-    # id, 암호화된pw을 가지고 해당 유저를 찾습니다.
-    result = db.user.find_one({'id': id_receive, 'pw': pw_hash})
-
-    # 찾으면 JWT 토큰을 만들어 발급합니다.
-    if result is not None:
-        # JWT 토큰에는, payload와 시크릿키가 필요합니다.
-        # 시크릿키가 있어야 토큰을 디코딩(=풀기) 해서 payload 값을 볼 수 있습니다.
-        payload = {
-            'id': id_receive,
-        }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
-
-        # token을 줍니다.
-        return jsonify({'result': 'success', 'token': token})
-    # 찾지 못하면
-    else:
-        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
-
-#####
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
-
