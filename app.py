1944eb6690238377d7afc38ab468e866e0d3923c
john <john3210of@gmail.com>
2022-01-11 22:59:40 +0900

    commit test

--- app.py
+++ app.py
@@ -24,28 +24,28 @@ import datetime
 def list_main():
     cursor = db.gameboard.find().sort('date', -1)  # date 역순(최근)
     result = dumps(list(cursor), ensure_ascii=False)
-    return render_template('index.html', items=result) # jinja 적용
+    return render_template('index.html', items=result)  # jinja 적용
 
 
 # 최근 날짜부터 보여주기
 @app.route('/api/list/dateOrder', methods=['GET'])
 def view_list_date_order():
-    all_lists = list(db.gameboard.find().sort('date',-1)) # date 역순(최근)
-    return jsonify({'all_lists': dumps(all_lists)}) # object id 사용하기 위해 dumps 사용
+    all_lists = list(db.gameboard.find().sort('date', -1))  # date 역순(최근)
+    return jsonify({'all_lists': dumps(all_lists)})  # object id 사용하기 위해 dumps 사용
 
 
 # 좋아요가 많은 순으로 보여주기
 @app.route('/api/list/likeOrder', methods=['GET'])
 def view_list_like_order():
-    all_lists = list(db.gameboard.find().sort('likes',-1)) # 좋아요 역순(많은 순)
-    return jsonify({'all_lists': dumps(all_lists)}) # object id 사용하기 위해 dumps 사용
+    all_lists = list(db.gameboard.find().sort('likes', -1))  # 좋아요 역순(많은 순)
+    return jsonify({'all_lists': dumps(all_lists)})  # object id 사용하기 위해 dumps 사용
 
 
 # 황금밸런스만 보여주기
 @app.route('/api/list/goldenBalance', methods=['GET'])
 def view_list_golden():
-    golden_lists = list(db.gameboard.find().sort('date',-1)) # date 역순(최근)
-    return jsonify({'all_lists': dumps(golden_lists)}) # object id 사용하기 위해 dumps 사용
+    golden_lists = list(db.gameboard.find().sort('date', -1))  # date 역순(최근)
+    return jsonify({'all_lists': dumps(golden_lists)})  # object id 사용하기 위해 dumps 사용
 
 
 # 상세 페이지 게시글에 관한 데이터 DB에서 받아오기
@@ -53,6 +53,7 @@ def view_list_golden():
 def list_detail():
     idx_receive = request.args.get('idx')
     post = db.posts.find_one({'_id': ObjectId(idx_receive)})
+
     percent_left = round((post['count_left'] / (post['count_left'] + post['count_right'])) * 100, 1)
     percent_right = round((post['count_right'] / (post['count_left'] + post['count_right'])) * 100, 1)
 
@@ -160,13 +161,15 @@ def api_regist():
     db.user.insert_one({'id': id_receive, 'pw': pw_hash, 'name': name_receive})
     return jsonify({'result': 'success'})
 
+
 # id 중복확인 api
 @app.route('/api/regist/check_dup', methods=['POST'])
 def check_dup():
     id_receive = request.form['id_give']
-    #중복 여부에따라 T/F로 return
+    # 중복 여부에따라 T/F로 return
     exists = bool(db.user.find_one({"id": id_receive}))
     return jsonify({'result': 'success', 'exists': exists})
+
 
 # 로그인 api
 @app.route('/api/login', methods=['POST'])
@@ -188,7 +191,7 @@ def api_login():
         payload = {
             'id': id_receive,
         }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')  #.decode('utf-8')
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')  # .decode('utf-8')
         # token을 줍니다.
         return jsonify({'result': 'success', 'token': token})
     else:
@@ -197,8 +200,8 @@ def api_login():
 
 # 이미지를 저장하는 서버경로와 및 저장을 허용하는 확장자를 분류합니다.
 # 로컬에서는 절대경로로 "/Users/mac_cloud/Desktop/images" 로 지정하여 사용하였습니다.
-BOARD_IMAGE_PATH = "C:/Users/john/Desktop/sparta/qwe/hanghae_D8/static"
-ALLOWED_EXTENSIONS = set(["png", "jpg", "jpeg", "gif"])
+BOARD_IMAGE_PATH = "/Users/john/Desktop/sparta/qwe/hanghae_D8/static/img"
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
 
 # 업로드 하는 이미지의 크기를 제한했으며, 최대 15MB까지 가능합니다.
 app.config['BOARD_IMAGE_PATH'] = BOARD_IMAGE_PATH
@@ -238,7 +241,6 @@ def upload_image():
 # [게임 작성] (Create)
 @app.route('/post', methods=['GET', 'POST'])
 def list_post():
-
     if request.method == "POST":
         # 사용자의 id를 보내줍니다.
         # 글 작성 시 작성자의 이름을 바꿀 수 없도록 readonly 속성을 부여하였습니다.
@@ -276,6 +278,7 @@ def list_post():
     else:
         # 아무런 입력이 없이 GET 방식으로 들어왔을때, 게임 작성 페이지로 전환해줍니다.
         return render_template("post.html")
+
 
 # [게시글 수정] (Update)
 @app.route("/edit", methods=["PATCH"])
@@ -324,6 +327,7 @@ def list_edit():
             flash("글 수정 권한이 없습니다.")
             return redirect(url_for("list_main"))
 
+
 # [게시글 삭제] (Delete)
 @app.route("/api/detail", methods=["DELETE"])
 def game_delete():
@@ -336,9 +340,5 @@ def game_delete():
     return redirect(url_for("game_lists"))
 
 
-
-
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
-
-
